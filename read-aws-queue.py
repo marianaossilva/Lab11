61841a2a4e3068b08790b508b72f34361d3261e7
marianaossilva <marianaossilva@gmail.com>
2015-12-07 11:43:37 +0000

    Create read-aws-queue.py

---
+++ read-aws-queue.py
@@ -0,0 +1,33 @@
+import sys
+import boto.sqs
+import boto.sqs.queue
+import urllib2
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+
+if (len(sys.argv) != 2):
+    sys.ext('Usage: python count-aws-msgs.py queue_name')
+
+queueName = sys.argv[1]
+
+response = urllib2.urlopen('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+awsKey = response.read().split(':')
+access_key_id = awsKey[0]
+secret_access_key = awsKey[1]
+
+conn = boto.sqs.connect_to_region('eu-west-1', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+
+queue = conn.get_queue(queueName)
+if (queue is not None):
+	for i  in range(1,queue.count()) :
+		try:
+			msgs = conn.receive_message(queue, number_messages=queue.count(),attributes='All')
+			if(len(msgs) > 0):
+				print('The message = ' + msgs[i-1].get_body())
+			else:
+				print('No message to be retrieved')
+		except Exception, e:
+			print(e)
+else:
+    print('Queue ' + queueName + ' not found')
